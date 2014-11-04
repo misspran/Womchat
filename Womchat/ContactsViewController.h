99e69955de4e908ad288f9b221fa7e105d9a806b
Ryan Siska <rsiska1@gmail.com>
2014-11-03 19:05:45 -0600

    FacebookFriend set up and in the process of setting up Contacts table views

---
+++ Womchat/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  Womchat
+//
+//  Created by S on 11/3/14.
+//  Copyright (c) 2014 Vi & Ryan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ContactsViewController : ViewController
+
+@end
